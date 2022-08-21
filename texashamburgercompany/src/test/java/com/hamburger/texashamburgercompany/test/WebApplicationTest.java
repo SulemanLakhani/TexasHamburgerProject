92fc06010d73445e281bf8f430546a7fc7eeb2bc
Suleman-Lakhani <sulemanlakhani72@gmail.com>
2022-08-20 21:18:04 -0400

    made changes to log4j2.xml

---
+++ texashamburgercompany/src/test/java/com/hamburger/texashamburgercompany/test/WebApplicationTest.java
@@ -0,0 +1,21 @@
+package com.hamburger.texashamburgercompany.test;
+
+import com.hamburger.texashamburgercompany.controller.RestaurantController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+public class WebApplicationTest {
+
+    @Autowired
+    private RestaurantController restaurantController;
+
+    @Test
+    public void contextLoads() throws Exception{
+        assertThat(restaurantController).isNotNull();
+
+    }
+}
